a403ddbd15d92e6713e37bacdcb702bb7fe11c6b
Boyderr <emilyllboyd@gmail.com>
2023-07-11 16:16:08 -0700

    adding project to github

---
+++ scripts/main.js
@@ -0,0 +1,17 @@
+// Get the necessary elements
+const menuOpenIcon = document.querySelector('.navbar__menu-open');
+const menuCloseIcon = document.querySelector('.navbar__menu-close');
+const menu = document.querySelector('.navbar__menu');
+
+// Add event listener to open the menu
+menuOpenIcon.addEventListener('click', () => {
+  menu.classList.add('open');
+});
+
+// Add event listener to close the menu
+menuCloseIcon.addEventListener('click', () => {
+  menu.classList.remove('open');
+});
+
+
+
